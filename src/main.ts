2766875020648dc5052a1a1b076598855a53f713
ricardosantos <rick.giometti@hotmail.com>
2021-09-19 17:14:45 -0300

    add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,    // strips extra tags from json
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
